4a21cc10907c4493ac773168e523bdce993a05ca
jtorresli <manu.jose.torres@gmail.com>
2020-05-06 15:30:36 -0500

    feat: indicando el tipo

--- Practica-1/ejercicio4.cpp
+++ Practica-1/ejercicio4.cpp
@@ -6,15 +6,71 @@ int main()
 {
   ifstream archivo;
   string palabra;
-  string evaluar="";
-  int es_numero=1 ;
-  int es_letra=1;
-  int es_especial=1;
+  string evaluar = "";
+  bool iniciar = true;
+  int es_numero = 0;
+  int es_letra = 0;
+  int es_especial = 0;
   archivo.open("codigo.cpp",ios::in);
 
   while(!archivo.eof()) {
     palabra = (char)archivo.get();
 
+    for (int i=0;i< palabra.size();i++) {
+
+      if (palabra[i]==' ' || palabra[i]=='\t' || palabra[i]=='\n') {
+        if (palabra[i]!='\n') {
+
+          if (es_letra) {
+              cout<<evaluar<<" ";
+              cout<<"Es una palabra\n";
+          }
+          if (es_numero) {
+              cout<<evaluar<<" ";
+              cout<<"Es un numero\n";
+          }
+          if (es_especial) {
+              cout<<evaluar<<" ";
+              cout<<"Es un caracter especial\n";
+          }
+          evaluar = "";
+          iniciar = true;
+        }
+      }
+      else {
+        evaluar+=palabra[i];  
+
+        if (iniciar) {
+          es_numero = 1;
+          es_letra = 1;
+          es_especial = 1;
+          iniciar = false;
+        }
+        
+        if ((palabra[i]>64 && palabra[i]<91) || (palabra[i]>96 && palabra[i]<123)) {
+            es_letra *= 1;    
+        }
+        else {
+            es_letra *= 0;    
+        }  
+        if (palabra[i]>47 && palabra[i]<58) {
+            es_numero *= 1;  
+            
+        }
+        else {
+            es_numero *= 0;    
+        }
+        if ( palabra[i]=='+' || palabra[i]=='-' || palabra[i]=='*' || palabra[i]=='\\') {
+            es_especial *= 1;  
+            
+        }
+        else {
+            es_especial *= 0;    
+        }
+ 
+      }
+    }
+    /*  
     for (int i=0;i< palabra.size();i++) {
       
       evaluar+=palabra[i];  
@@ -66,7 +122,7 @@ int main()
         }
         
       }  
-    }
+    }*/
   }
    
    return 0;
